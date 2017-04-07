342507f7fcfe0b8595aedf6d1fd635829a4a1ca6
Junxiao Shi <git@mail1.yoursunny.com>
2017-04-07 20:10:26 +0000

    hashtable: resize test case

--- tests/hashtable.t.c
+++ tests/hashtable.t.c
@@ -24,8 +24,42 @@ void test_basic(void)
   Hashtable_dtor(ht);
 }
 
+void test_resize(void)
+{
+  char s[5];
+  Hashtable* ht = Hashtable_new();
+  size_t nBuckets1 = ht->nBuckets;
+
+  for (int i = 0; i < 4096; ++i) {
+    snprintf(s, sizeof(s), "%x", i);
+    CU_ASSERT_TRUE(Hashtable_insert(ht, s));
+  }
+
+  size_t nBuckets2 = ht->nBuckets;
+  CU_ASSERT(nBuckets2 > nBuckets1);
+
+  for (int i = 0; i < 4096; ++i) {
+    snprintf(s, sizeof(s), "%x", i);
+    CU_ASSERT_TRUE(Hashtable_has(ht, s));
+  }
+
+  size_t nBuckets3 = ht->nBuckets;
+  CU_ASSERT(nBuckets3 == nBuckets2);
+
+  for (int i = 0; i < 4096; ++i) {
+    snprintf(s, sizeof(s), "%x", i);
+    CU_ASSERT_TRUE(Hashtable_erase(ht, s));
+  }
+
+  size_t nBuckets4 = ht->nBuckets;
+  CU_ASSERT(nBuckets4 < nBuckets3);
+
+  Hashtable_dtor(ht);
+}
+
 void suite_hashtable(void)
 {
   CU_pSuite suite = CU_add_suite("hashtable", NULL, NULL);
   CU_add_test(suite, "basic", test_basic);
+  CU_add_test(suite, "resize", test_resize);
 }
